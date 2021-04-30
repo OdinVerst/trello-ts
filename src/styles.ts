c8d0956b1f8998eb372961cc9c6b55f4cf26760c
Sergey Razdolyanskiy <grafcorp01@gmail.com>
2021-04-30 17:53:28 +0300

    Add all styles

--- src/styles.ts
+++ src/styles.ts
@@ -9,3 +9,28 @@ export const AppContainer = styled.div`
   padding: 20px;
   background-color: #3179ba;
 `;
+
+export const ColumnContainer = styled.div`
+  flex-grow: 0;
+  width: 300px;
+  min-height: 40px;
+  margin-right: 20px;
+  padding: 8px 8px;
+  background-color: #ebecf0;
+  border-radius: 3px;
+`;
+
+export const ColumnTitle = styled.div`
+  padding: 6px 16px 12px;
+  font-weight: bold;
+`;
+
+export const CardContainer = styled.div`
+  max-width: 300px;
+  margin-bottom: 0.5rem;
+  padding: 0.5rem 1rem;
+  background-color: #fff;
+  border-radius: 3px;
+  box-shadow: #091e4240 0 1px 0 0;
+  cursor: pointer;
+`
